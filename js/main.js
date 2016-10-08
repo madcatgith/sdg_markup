8cfccc300d8cd9dc4521620ca665c6453e930217
MaDCaT <wasd.exe@gmail.com>
2016-10-08 20:15:38 +0300

    First complete version

--- js/main.js
+++ js/main.js
@@ -7,4 +7,30 @@ $(document).ready(function(){
 			$(this).find('.zoom').hide();
 		}
 	);
+
+	$('#slider').bxSlider();
+	$(".fbox").fancybox();
+
+	$('#price .button').on('click',function(e){
+		e.preventDefault();
+		$('.popup').toggle();
+		$('.overlay').toggle();
+	});
+	$('#close_cross').on('click',function(e){
+		e.preventDefault();
+		$('.popup').hide();
+		$('.overlay').hide();
+	});
+	$('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
